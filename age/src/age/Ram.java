293b341362cb01a76d7fce9d9508f656a12ac387
rambhadake <TECHHUB>
2025-04-14 11:38:32 +0530

    initial commit

---
+++ age/src/age/Ram.java
@@ -0,0 +1,20 @@
+package age;
+
+import java.util.Scanner;
+
+public class Ram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc = new Scanner(System.in);
+       int age = sc.nextInt();
+       
+       if(age > 20) {
+    	   System.out.println("Adult");
+       }
+       else {
+    	   System.out.println("Not-Adult");
+       }
+	}
+
+}
